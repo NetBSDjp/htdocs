2d5e0d37f3da57261af85aebcc26011ab676c1ad
soda <soda>
1999-01-19 14:46:22 +0000

    add the descrptions that checker field should be set "NOT-YET" *after* translation is done.

--- JP/staff/www-pending.sh
+++ JP/staff/www-pending.sh
@@ -27,10 +27,23 @@ X	<H2>登録判断待ちファイル</H2>
 X	<UL>
 _EOF_
 
-awk '/^[^#]/ && $3 != "" && $3 != "NOT-YET" {
+awk '/^[^#]/ && $3 != "" && $3 != "NOT-YET" && $3 !~ ",DONE$" {
     printf "<LI><A HREF=\"../../../ja/%s\">%s</a>\n", $1, $1
     printf "   (<A HREF=\"http://www.jp.netbsd.org/%s\">原文</A>)\n", $1
     printf "   - 翻訳: %s, 査読: %s\n", $2, $3
+}' translation-assign.txt
+
+sed 's/^X	//' << '_EOF_'
+X	</UL>
+X	
+X	<HR>
+X	<H2>翻訳中のファイル</H2>
+X	<UL>
+_EOF_
+
+awk '/^[^#]/ && $2 != "" && $3 == "" {
+    printf "<LI><A HREF=\"http://www.jp.netbsd.org/%s\">%s (原文)</a>\n", $1, $1
+    printf "   - 翻訳: %s\n", $2
 }' translation-assign.txt
 
 sed 's/^X	//' << '_EOF_'
