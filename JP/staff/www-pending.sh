2a587844a2edef4d05b23143a808720601db4e29
sakamoto <sakamoto>
1999-03-26 10:25:38 +0000

    Add cvsweb.cgi links. Use TABLE tag.

--- JP/staff/www-pending.sh
+++ JP/staff/www-pending.sh
@@ -11,43 +11,50 @@ X
 X	<BODY>
 X	
 X	<H2>査読待ちファイル</H2>
-X	<UL>
+X	<TABLE BORDER="1">
+X	<TR><TH>ファイル <TH>CVS <TH>原文 <TH>翻訳者
 _EOF_
 
 awk '/^[^#]/ && $3 == "NOT-YET" {
-    printf "<LI><A HREF=\"../../../ja/%s\">%s</a>\n", $1, $1
-    printf "   (<A HREF=\"http://www.jp.netbsd.org/%s\">原文</A>)\n", $1
-    printf "   - 翻訳: %s\n", $2
+    printf "<TR><TD><A HREF=\"../../%s\">%s</a>\n", $1, $1
+    printf "    <TD><A HREF=\"http://www.jp.netbsd.org/cgi-bin/cvsweb.cgi/htdocs/ja/%s\">CVS</A>\n", $1
+    printf "    <TD><A HREF=\"http://www.jp.netbsd.org/%s\">原文</A>\n", $1
+    printf "    <TD>%s\n", $2
 }' translation-assign.txt
 
 sed 's/^X	//' << '_EOF_'
-X	</UL>
+X	</TABLE>
 X	
 X	<H2>登録判断待ちファイル</H2>
-X	<UL>
+X	<TABLE BORDER="1">
+X	<TR><TH>ファイル <TH>CVS <TH>原文 <TH>翻訳者 <TH>査読者
 _EOF_
 
 awk '/^[^#]/ && $3 != "" && $3 != "NOT-YET" && $3 !~ ",DONE$" {
-    printf "<LI><A HREF=\"../../../ja/%s\">%s</a>\n", $1, $1
-    printf "   (<A HREF=\"http://www.jp.netbsd.org/%s\">原文</A>)\n", $1
-    printf "   - 翻訳: %s, 査読: %s\n", $2, $3
+    printf "<TR><TD><A HREF=\"../../%s\">%s</a>\n", $1, $1
+    printf "    <TD><A HREF=\"http://www.jp.netbsd.org/cgi-bin/cvsweb.cgi/htdocs/ja/%s\">CVS</A>\n", $1
+    printf "    <TD><A HREF=\"http://www.jp.netbsd.org/%s\">原文</A>\n", $1
+    printf "    <TD>%s\n", $2
+    printf "    <TD>%s\n", $3
 }' translation-assign.txt
 
 sed 's/^X	//' << '_EOF_'
-X	</UL>
+X	</TABLE>
 X	
 X	<HR>
 X	<H2>翻訳中のファイル</H2>
-X	<UL>
+X	<TABLE BORDER="1">
+X	<TR><TH>ファイル <TH>原文 <TH>翻訳者
 _EOF_
 
 awk '/^[^#]/ && $2 != "" && $3 == "" {
-    printf "<LI><A HREF=\"http://www.jp.netbsd.org/%s\">%s (原文)</a>\n", $1, $1
-    printf "   - 翻訳: %s\n", $2
+    printf "<TR><TD><A HREF=\"../../%s\">%s</a>\n", $1, $1
+    printf "    <TD><A HREF=\"http://www.jp.netbsd.org/%s\">原文</A>\n", $1
+    printf "    <TD>%s\n", $2
 }' translation-assign.txt
 
 sed 's/^X	//' << '_EOF_'
-X	</UL>
+X	</TABLE>
 X	</BODY>
 X	</HTML>
 _EOF_
