7df89ed930676d070d5a7cac67f81c17021f59d2
kano <kano>
2002-02-14 14:28:21 +0000

    Sync with TNF index.html: 1.646 Makefile: 1.3 makeindex.sh: 1.1

--- ja/makeindex.sh
+++ ja/makeindex.sh
@@ -2,15 +2,15 @@
 #
 # $NetBSD: makeindex.sh,v 1.1 2002/02/14 03:22:23 jschauma Exp $
 #
-# update index.html with NEWS and EVENTS from /htdocs/Changes/index.html and
-# /htdocs/gallery/events.html
+# update index.html with NEWS and EVENTS from /htdocs/ja/Changes/index.html and
+# /htdocs/ja/gallery/events.html
 
 # insert the head
 sed -e '/<!-- NEWS::START -->/q' index.html > index.tmp
 
 # links to anchors on that page are new, trim grep's separator and only print
 # the first 8 links (each links is two lines)
-grep -B 1 'href="#' Changes/index.html | sed -e 's/<font face="helvetica, arial, sans-serif">/<font face="helvetica, arial" size="-1">/' -e 's/href="#/href="Changes\/#/' -e '/^--/d' | sed -e '17,$d' >> index.tmp
+grep -B 1 'href="#' Changes/index.html | sed -e 's/^<li>$/<li><font size="-1">/' -e 's/<\/li>$/<\/font><\/li>/' -e 's/href="#/href="Changes\/#/' -e '/^--/d' | sed -e '17,$d' >> index.tmp
 
 # insert the middle part
 sed -e '/<!-- NEWS::END -->/!d' -e '/<!-- NEWS::END -->/bt
@@ -28,7 +28,7 @@ sed -e '/<!-- NEWS::END -->/!d' -e '/<!-- NEWS::END -->/bt
 # links to anchors on that page are events, if we encounter "--" it means that
 # Upcoming Events is over (grep had to jump) and we can stop.  Only add at
 # most 6 events (each event is two lines)
-grep -B1 'href="#' gallery/events.html | sed -e 's/<font face="helvetica, arial, sans-serif">/<font face="helvetica, arial" size="-1">/' -e 's/href="#/href="gallery\/events.html#/' -e '/^--/q' | sed -e '/^--/d' -e '13,$d' >> index.tmp
+grep -B1 'href="#' gallery/events.html | sed -e 's/^<li>$/<li><font size="-1">/' -e 's/<\/li>$/<\/font><\/li>/' -e 's/href="#/href="gallery\/events.html#/' -e '/^--/q' | sed -e '/^--/d' -e '13,$d' >> index.tmp
 
 # insert the tail
 sed -e '/<!-- EVENTS::END -->/!d' -e '/<!-- EVENTS::END -->/bt
