0fd9b566ac8c42b94b3f8d3844c92e889f7191d8
kano <kano>
2006-06-04 06:16:19 +0000

    import from TNF.   Ports/news68k/faq.list: 1.9   Ports/pdp10/index.html: 1.13   Releases/formal-1.5/index.list: 1.31   gallery/in-Action/index.act-template: 1.51   list2html.pl: 1.97

--- ja/list2html.pl
+++ ja/list2html.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/env perl
 #
-# $NetBSD: list2html.pl,v 1.96 2004/10/30 22:33:23 jschauma Exp $
+# $NetBSD: list2html.pl,v 1.97 2006/05/31 11:52:09 kano Exp $
 # Process *.list files into indexed *.html files. (abs)
 # Looks for these compulsary tags:
 #	<LIST>			Include generated list of entries here.
@@ -68,7 +68,7 @@ my(%months) = ('Jan' => 1,	'Feb' => 2,	'Mar' => 3,
 $months_previous = 13;	# Previous months to display for DATE entries
 $list_date_links = 8;	# List the first N date entries on stdout
 
-$version = '$Revision: 1.96 $';
+$version = '$Revision: 1.97 $';
 $version =~ /([\d.]+)/ && ($version = $1);
 
 if (!&getopts('a:c:dm:qhV', \%opt) || $opt{'h'} || ( !$opt{'V'} && @ARGV != 2) )
@@ -570,6 +570,7 @@ sub makelist
 
     close(FILE);
     $list .= "</ul>\n";
+    $list =~ s#<ul>\n</ul>\n##g;
     if (!$endlist)
 	{ &warn("Unable to locate </LIST> tag, check header to see if this is desired\n"); }
     if ($data !~ s/<LIST>/$list/)
